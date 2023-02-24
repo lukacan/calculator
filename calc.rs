47b89b77e303a52ed6c8ca0f6887e8e2379eb4ff
lukacan <andrej.lukacovic98@gmail.com>
2023-02-24 14:25:59 +0100

    operations done

--- calc.rs
+++ calc.rs
@@ -1,14 +1,88 @@
-// This is a comment, and is ignored by the compiler
-// You can test this code by clicking the "Run" button over there ->
-// or if you prefer to use your keyboard, you can use the "Ctrl + Enter" shortcut
+use std::io;
 
-// This code is editable, feel free to hack it!
-// You can always return to the original code by clicking the "Reset" button ->
 
-// This is the main function
+
+trait ComputeFns{
+    fn addition(&self)->f64;
+    fn subtraction(&self)->f64;
+    fn multiplication(&self)->f64;
+    fn division(&self)->f64;
+    fn reminder(&self)->f64;
+}
+
+
+struct Compute{
+    x: f64,
+    y: f64,
+}
+
+impl ComputeFns for Compute{
+    fn addition(&self)->f64{
+        self.x + self.y
+    }
+    fn subtraction(&self)->f64{
+        self.x - self.y
+    }
+    fn multiplication(&self)->f64{
+        self.x * self.y
+    }
+    fn division(&self)->f64{
+        if self.y == 0.0{
+            f64::NAN
+        }
+        else{
+            self.x / self.y
+        }
+    }
+    fn reminder(&self)->f64{
+        if self.y == 0.0{
+            f64::NAN
+        }
+        else{
+            self.x % self.y
+        }
+    }
+
+}
+
+fn read_input(){
+    let mut input_string: String = String::new();
+    let mut inputs: Vec<f64> = Vec::new();
+
+
+    loop{
+        println!("Please type exactly two numbers (format: X Y):");
+        match io::stdin().read_line(&mut input_string) {
+            Ok(n)=>{
+                inputs=input_string.trim().split(" ")
+                .map(|x| x.parse().expect("NUT A NUMBER!")).collect();
+
+                match inputs.len(){
+                    2=>{
+                        println!("You wrote {:?}", inputs,);
+                        let example = Compute { x:inputs[0],
+                                                y:inputs[1]};
+                        println!("Addition: {}\nSubtraction: {}\nMultiplication: {}\nDivision: {}\nReminder: {}", 
+                                example.addition(),example.subtraction(),
+                                example.multiplication(),example.division(),
+                                example.reminder());
+                        
+                    }
+                    _=>{
+                        println!("STUPID");
+                    }
+                }
+            }
+            Err(error)=>{
+
+            }
+        }
+    }
+    
+
+}
+
+
 fn main() {
-    // Statements here are executed when the compiled binary is called
-
-    // Print text to the console
-    println!("Hello World!");
+    read_input();
 }
