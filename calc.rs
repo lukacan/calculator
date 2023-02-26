16ff22cf266c748a933c2b0d40c8a8bfd7bcd724
lukacan <andrej.lukacovic98@gmail.com>
2023-02-26 12:11:12 +0100

    comments added

--- calc.rs
+++ calc.rs
@@ -1,25 +1,22 @@
 use std::io;
-
-
-pub trait ComputeFns{
-    fn addition(&self)->f64;
-    fn subtraction(&self)->f64;
-    fn multiplication(&self)->f64;
-    fn division(&self)->f64;
-    fn modulo(&self)->f64;
-}
-
 mod secret{
-    use ComputeFns;
+    trait ComputeFns{
+        fn addition(&self)->f64;
+        fn subtraction(&self)->f64;
+        fn multiplication(&self)->f64;
+        fn division(&self)->f64;
+        fn modulo(&self)->f64;
+    }
     pub struct Compute{
         x: f64,
         y: f64,
     }
     impl Compute{
-        pub fn new(arg1:f64,arg2:f64)->Self
+        pub fn new(arg1:&f64,arg2:&f64)->Self
         {
-            Self {x:arg1,y:arg2}
+            Self {x:*arg1,y:*arg2}
         }
+
     }
     impl ComputeFns for Compute{
         fn addition(&self)->f64{
@@ -48,7 +45,16 @@ mod secret{
             }
         }
     }
+    pub fn print_results(example: &Compute){
+        println!("Numbers entered X: {}, Y: {}",example.x,example.y);
+        println!("Addition (X + Y): {}",example.addition());
+        println!("Subtraction (X - Y): {}",example.subtraction());
+        println!("Multiplication (X * Y): {}",example.multiplication());
+        println!("Division (X / Y): {}",example.division());
+        println!("Modulo (X mod Y): {}",example.modulo()); 
+    }
 }
+
 fn read_num(read_into:&mut f64){
     let mut input_string: String = String::new();
     loop{
@@ -58,6 +64,7 @@ fn read_num(read_into:&mut f64){
             Ok(_n) =>{
                 match input_string.trim().parse::<f64>(){
                     Ok(f)=>{
+                        // change here
                         *read_into = f;
                     }
                     _=>{
@@ -74,22 +81,28 @@ fn read_num(read_into:&mut f64){
     }
 }
 fn calc(){
-
+    // declare here
     let mut x_in: f64 = 0.0;
     let mut y_in: f64 = 0.0;
 
+    // pass here as mutable reference,
+    // needs to be mutable in order to change it
+    // so basically we not changing ownership, just borrowing and telling
+    // the varaible can be changed
     read_num(&mut x_in);
     read_num(&mut y_in);
 
-    let example = secret::Compute::new(x_in,y_in);
-    println!("Numbers entered X: {}, Y: {}",x_in,y_in);
-    println!("Addition (X + Y): {}",example.addition());
-    println!("Subtraction (X - Y): {}",example.subtraction());
-    println!("Multiplication (X * Y): {}",example.multiplication());
-    println!("Division (X / Y): {}",example.division());
-    println!("Modulo (X mod Y): {}",example.modulo()); 
-}
 
+    // call public constructor, x_in and y_in are borrowed only
+    // without mutability
+    let example = secret::Compute::new(&x_in,&y_in);
+    
+    // won`t work, no visible
+    //println!("Addition (X + Y): {}",example.addition());
+
+    // pass as immutable ref, so borrow only without rights to change
+    secret::print_results(&example);
+}
 
 fn main() {
     calc();
